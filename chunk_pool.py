310b176d0758322bf472f3172cc99a50422781c7
Glen (Mac) <>
2018-03-25 10:35:45 -0400

    ChunkPool class

---
+++ chunk_pool.py
@@ -0,0 +1,19 @@
+from hex_chunk import *
+
+class ChunkPool(object):
+    POOL_SIZE = 12
+    pool = []
+
+    def __init__(self):
+        for i in range(self.POOL_SIZE):
+            self.pool.append(HexChunk(Vector2(0, 0)))
+
+    def take_from_pool(self, position):
+        chunk = self.pool.pop()
+        chunk.position = position
+        chunk.coordinates = HexCoordinates().from_position(chunk.position)
+        return chunk
+
+    def return_to_pool(self, chunk):
+        chunk.clear()
+        self.pool.append(chunk)
